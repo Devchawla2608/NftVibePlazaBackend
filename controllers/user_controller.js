876ae0ca17932b463436a1161a577c0ee144575e
Deepanshu Chawla <dev.chawla2608@gmail.com>
2023-08-11 21:55:06 +0530

    Error 1

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,4 @@
-const User = require("../models/user.js");
+const User = require("../models/User.js");
 const jwt  = require('jsonwebtoken')
 module.exports.create = function (req, res) {
     if (req.body.body.password !== req.body.body.confirmPassword) {
